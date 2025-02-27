2aea6e745343949313d737cd191239bf85d9ad59
Paul Abel <p.abel@f5.com>
2025-02-27 16:54:32 +0000

    add event debug

--- tests/suite/utils/resources_utils.py
+++ tests/suite/utils/resources_utils.py
@@ -1759,6 +1759,7 @@ def wait_for_event(v1: CoreV1Api, text, namespace, retry=30, interval=1) -> None
     while c < retry:
         events = get_events(v1, namespace)
         for i in range(len(events) - 1, -1, -1):
+            print(i)
             if text in events[i].message:
                 return True
         wait_before_test(interval)
