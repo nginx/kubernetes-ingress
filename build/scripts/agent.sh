c63337ae3476dfc18da45441815b8fd5b1dfae8d
Paul Abel <p.abel@f5.com>
2025-04-01 14:26:52 +0100

    removal of agent toggle flags

--- build/scripts/agent.sh
+++ build/scripts/agent.sh
@@ -3,3 +3,16 @@
 set -e
 
 chown -R 101:0 /etc/nginx-agent
+if [ -f "/opt/app_protect/RELEASE" ]; then
+    # shellcheck disable=SC2002
+    if cat "/opt/app_protect/RELEASE" | cut -d '.' -f 1 | grep -q 4; then
+        NAP_VERSION=$(cat /opt/app_protect/VERSION)
+        echo "Adding NAP $NAP_VERSION directories"
+
+        mkdir -p /etc/ssl/nms /opt/nms-nap-compiler
+        chown -R 101:0 /etc/ssl/nms /opt/nms-nap-compiler
+        chmod -R g=u /etc/ssl/nms /opt/nms-nap-compiler
+
+        ln -s /opt/app_protect "/opt/nms-nap-compiler/app_protect-${NAP_VERSION}"
+    fi
+fi
