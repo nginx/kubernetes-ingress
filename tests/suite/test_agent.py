dde207a819a676163649d9915b11a6fc27abf51f
Paul Abel <p.abel@f5.com>
2025-04-07 14:45:34 +0100

    make agent tests run on all images

--- tests/suite/test_agent.py
+++ tests/suite/test_agent.py
@@ -4,7 +4,7 @@ from suite.utils.resources_utils import get_first_pod_name, wait_before_test
 
 
 @pytest.mark.skip_for_nginx_oss
-@pytest.mark.agent
+@pytest.mark.appprotect
 @pytest.mark.parametrize(
     "crd_ingress_controller_with_ap",
     [
@@ -18,7 +18,7 @@ from suite.utils.resources_utils import get_first_pod_name, wait_before_test
     ],
     indirect=["crd_ingress_controller_with_ap"],
 )
-class TestAppProtectAgent:
+class TestAppProtectAgentV2:
     def test_ap_agent(self, kube_apis, ingress_controller_prerequisites, crd_ingress_controller_with_ap):
         pod_name = get_first_pod_name(kube_apis.v1, "nginx-ingress")
         log = kube_apis.v1.read_namespaced_pod_log(pod_name, ingress_controller_prerequisites.namespace)
@@ -47,4 +47,47 @@ class TestAppProtectAgent:
         result_conf = str(resp)
 
         assert f"Failed to get nginx-agent version: fork/exec /usr/bin/nginx-agent" not in log
-        assert "nginx-agent version " in result_conf
+        assert "nginx-agent version v2" in result_conf
+
+
+@pytest.mark.parametrize(
+    "ingress_controller",
+    [
+        {
+            "extra_args": [
+                f"-agent=true",
+            ]
+        }
+    ],
+    indirect=["ingress_controller"],
+)
+class TestAgentV3:
+    def test_ap_agent(self, kube_apis, ingress_controller_prerequisites, ingress_controller):
+        pod_name = get_first_pod_name(kube_apis.v1, "nginx-ingress")
+        log = kube_apis.v1.read_namespaced_pod_log(pod_name, ingress_controller_prerequisites.namespace)
+
+        command = ["/usr/bin/nginx-agent", "-v"]
+        retries = 0
+        while retries <= 3:
+            wait_before_test()
+            try:
+                resp = stream(
+                    kube_apis.v1.connect_get_namespaced_pod_exec,
+                    pod_name,
+                    ingress_controller_prerequisites.namespace,
+                    command=command,
+                    stderr=True,
+                    stdin=False,
+                    stdout=True,
+                    tty=False,
+                )
+                break
+            except Exception as e:
+                print(f"Error: {e}")
+                retries += 1
+                if retries == 3:
+                    raise e
+        result_conf = str(resp)
+
+        assert f"Failed to get nginx-agent version: fork/exec /usr/bin/nginx-agent" not in log
+        assert "nginx-agent version v3" in result_conf
