fcdfbfc958dfe0bce3e90caefa6977bda03e2330
Paul Abel <p.abel@f5.com>
2025-04-28 13:59:20 +0100

    get auth working

--- .github/workflows/test.py
+++ .github/workflows/test.py
@@ -1,4 +1,7 @@
-from github import Github
+import os
+
+# Authentication is defined via github.Auth
+from github import Auth, Github
 
 # Authentication using personal access token
 GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
@@ -6,13 +9,20 @@ if not GITHUB_TOKEN:
     print("Error: GITHUB_TOKEN not set")
     exit(1)
 
+# using an access token
+auth = Auth.Token(GITHUB_TOKEN)
+
+
 def main():
-    github_client = Github(GITHUB_TOKEN)
-    
+    client = Github(auth=auth)
+
     # List all repositories available to the token's user
     print("Fetching repositories:")
-    for repo in github_client.get_user().get_repos():
+    for repo in client.get_user().get_repos():
         print(repo.full_name)
-        
+
+    client.close()
+
+
 if __name__ == "__main__":
     main()
