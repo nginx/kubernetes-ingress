05b20a97050d547e51e2b65a5b3337a1b8dfca3b
Paul Abel <p.abel@f5.com>
2025-04-28 14:49:49 +0100

    added PyYaml to parse the release.yml file

--- .github/workflows/test.py
+++ .github/workflows/test.py
@@ -1,7 +1,19 @@
 import os
 
+from yaml import dump, load
+
+try:
+    from yaml import CDumper as Dumper
+    from yaml import CLoader as Loader
+except ImportError:
+    from yaml import Dumper, Loader
+
 # Authentication is defined via github.Auth
 from github import Auth, Github
+
+# Constants
+nic_repo = "nginx/kubernetes-ingress"
+release_yaml = ".github/release.yml"
 
 # Authentication using personal access token
 GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
@@ -13,15 +25,47 @@ if not GITHUB_TOKEN:
 auth = Auth.Token(GITHUB_TOKEN)
 
 
+def paginated_list_contains(list, search_string):
+    for item in list:
+        if search_string in item.full_name:
+            return True
+    return False
+
+
+def get_pull_requests(client, repo_name):
+    repo = client.get_repo(repo_name)
+    pull_requests = repo.get_pulls(state="open", sort="created", base="main")
+    return pull_requests
+
+
 def main():
     client = Github(auth=auth)
 
-    # List all repositories available to the token's user
-    print("Fetching repositories:")
-    for repo in client.get_user().get_repos():
-        print(repo.full_name)
+    if paginated_list_contains(client.get_user().get_repos(), nic_repo):
+        print(f"User has access to {nic_repo}")
+    else:
+        print(f"User does not have access to {nic_repo}")
+
+    # for pr in get_pull_requests(client, nic_repo):
+    #     print(f"Pull request #{pr.number} - {pr.title}")
+    #     print(f"Created by: {pr.user.login}")
+    #     print(f"URL: {pr.html_url}")
+    #     print(f"Created at: {pr.created_at}")
+    #     print(f"Updated at: {pr.updated_at}")
+    #     print(f"State: {pr.state}")
+    #     print(f"Labels: {[label.name for label in pr.labels]}")
+    #     print("-" * 40)
 
     client.close()
+
+    with open(release_yaml) as file:
+        content = file.read()
+        parsed_yaml = load(content, Loader=Loader)
+        print(f"Parsed YAML content: {parsed_yaml}")
+        for category in parsed_yaml["changelog"]["categories"]:
+            print(f"Category: {category['title']}")
+            for label in category["labels"]:
+                print(f"  Label: {label}")
 
 
 if __name__ == "__main__":
