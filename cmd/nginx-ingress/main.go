d4745e8f10977442277ec7c70be5db0ec55e1232
Paul Abel <p.abel@f5.com>
2025-04-29 13:51:51 +0100

    rename mgmt trusted secret function

--- cmd/nginx-ingress/main.go
+++ cmd/nginx-ingress/main.go
@@ -166,7 +166,7 @@ func main() {
 			logEventAndExit(ctx, eventRecorder, pod, secretErrorReason, err)
 		}
 
-		if err := processTrustedCertSecret(kubeClient, nginxManager, mgmtCfgParams, controllerNamespace); err != nil {
+		if err := processMgmtTrustedCertSecret(kubeClient, nginxManager, mgmtCfgParams, controllerNamespace); err != nil {
 			logEventAndExit(ctx, eventRecorder, pod, secretErrorReason, err)
 		}
 
@@ -371,7 +371,7 @@ func processClientAuthSecret(kubeClient *kubernetes.Clientset, nginxManager ngin
 	return nil
 }
 
-func processTrustedCertSecret(kubeClient *kubernetes.Clientset, nginxManager nginx.Manager, mgmtCfgParams *configs.MGMTConfigParams, controllerNamespace string) error {
+func processMgmtTrustedCertSecret(kubeClient *kubernetes.Clientset, nginxManager nginx.Manager, mgmtCfgParams *configs.MGMTConfigParams, controllerNamespace string) error {
 	if mgmtCfgParams.Secrets.TrustedCert == "" {
 		return nil
 	}
