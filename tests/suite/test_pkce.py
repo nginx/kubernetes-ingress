ca4b0daab8bc592ac0edfdf72ebe04caf8d038cb
Gabor Javorszky <g.javorszky@f5.com>
2025-05-16 19:29:49 +0200

    pkce test fixture should also be class scoped

--- tests/suite/test_pkce.py
+++ tests/suite/test_pkce.py
@@ -44,7 +44,7 @@ class KeycloakSetupPKCE:
         self.secret = secret
 
 
-@pytest.fixture(scope="function")
+@pytest.fixture(scope="class")
 def keycloak_setup(request, kube_apis, test_namespace, ingress_controller_endpoint, virtual_server_setup):
 
     # Create Keycloak resources and setup Keycloak idp with PKCE enabled
