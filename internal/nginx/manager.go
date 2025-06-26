1383dacfa8b15057045c8b7248b22cf2e94556e5
Alex Fenlon <a.fenlon@f5.com>
2025-06-26 15:33:04 +0100

    update n1c labels the new required ones

--- internal/nginx/manager.go
+++ internal/nginx/manager.go
@@ -634,10 +634,10 @@ func (lm *LocalManager) AgentStart(agentDone chan error, instanceGroup string) {
 			fmt.Sprintf("product-type=%s", metadataInfo.ProductType),
 			fmt.Sprintf("product-version=%s", metadataInfo.ProductVersion),
 			fmt.Sprintf("cluster-id=%s", metadataInfo.ClusterID),
-			fmt.Sprintf("installation-name=%s", metadataInfo.InstallationName),
 			fmt.Sprintf("installation-id=%s", metadataInfo.InstallationID),
+			fmt.Sprintf("control-name=%s", metadataInfo.InstallationName),
 			fmt.Sprintf("control-id=%s", metadataInfo.InstallationID), // control-id is required but is the same as installation-id
-			fmt.Sprintf("installation-namespace=%s", metadataInfo.InstallationNamespace),
+			fmt.Sprintf("control-namespace=%s", metadataInfo.InstallationNamespace),
 		}
 		metadataLabels := "--labels=" + strings.Join(labels, ",")
 		args = append(args, metadataLabels)
