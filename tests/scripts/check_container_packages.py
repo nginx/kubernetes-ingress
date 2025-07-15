026c8938c8eae28b7a4fd1b54683ee585373bbd0
Paul Abel <p.abel@f5.com>
2025-07-15 16:19:49 +0100

    allow tag to be passed as cli arg

--- tests/scripts/check_container_packages.py
+++ tests/scripts/check_container_packages.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python
 
+import argparse
 import json
 import os
 import re
 
 import docker
+import docker.errors
+
+# parse args
+parser = argparse.ArgumentParser()
+parser.add_argument("-t", "--tag", type=str, help="NGINX Ingress Controller image tag", default="edge")
+args = parser.parse_args()
 
 client = docker.from_env()
 script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -13,11 +20,38 @@ with open(f"{script_dir}/../data/modules/data.json") as file:
 
     for image in images["images"]:
         regexInstalled = image["regex"]
+        tag = f"{args.tag}{image['tag_suffix']}"
+        try:
+            client.images.get(f"{image['image']}:{tag}")
+            print(f"Image {image['image']}:{tag} already exists, skipping pull")
+        except docker.errors.ImageNotFound:
+            print(f"Image {image['image']}:{tag} not found, pulling...")
+            ## pull the image
+            print(f"Pulling image {image['image']}:{tag} for platform {image['platform']}")
+            i = client.images.pull(repository=image["image"], tag=tag, platform=image["platform"])
+            print(f"Image {i.id} pulled successfully")
         for package in image["packages"]:
             command = f"{image['cmd']} {package['name']}"
-            output = client.containers.run(
-                image["image"], command, entrypoint="", platform=image["platform"], auto_remove=True, detach=False
-            )
+            output = ""
+            try:
+                output = client.containers.run(
+                    f"{image['image']}:{tag}",
+                    command,
+                    entrypoint="",
+                    platform=image["platform"],
+                    auto_remove=True,
+                    detach=False,
+                )
+            except (docker.errors.ContainerError, docker.errors.NotFound) as e:
+                print(f"Container error: {e}, retrying")
+                output = client.containers.run(
+                    f"{image['image']}:{tag}",
+                    command,
+                    entrypoint="",
+                    platform=image["platform"],
+                    auto_remove=True,
+                    detach=False,
+                )
             result = re.search(regexInstalled, output.decode("utf-8").strip())
             assert result, f"{package['name']} not found in {image['image']}, output: {output.decode('utf-8').strip()}"
             assert result.group(2).startswith(
