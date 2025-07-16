86f6610f038445fc01d30592375c682523cbd42b
Paul Abel <p.abel@f5.com>
2025-07-16 11:21:18 +0100

    check each image architecture

--- tests/scripts/check_container_packages.py
+++ tests/scripts/check_container_packages.py
@@ -53,48 +53,49 @@ with open(f"{script_dir}/../data/modules/data.json") as file:
     images = json.load(file)
 
     for image in images["images"]:
-        regexInstalled = systems[image["system"]]["regex"]
-        cmd = systems[image["system"]]["cmd"]
-        tag = f"{args.tag}{image['tag_suffix']}"
-        try:
-            i = client.images.get(f"{image['image']}:{tag}")
-            ## check if the image is for the correct platform
-            if image["platform"] != i.attrs["Os"]:
-                raise docker.errors.ImageNotFound(
-                    f"Image {image['image']}:{tag} is not for platform {image['platform']}, found {i.attrs['Os']}"
-                )
-        except docker.errors.ImageNotFound:
-            ## pull the image
-            logger.debug(f"Pulling image {image['image']}:{tag} for platform {image['platform']}")
-            i = client.images.pull(repository=image["image"], tag=tag, platform=image["platform"])
-            logger.debug(f"Image {i.id} pulled successfully")
-        for package in image["packages"]:
-            command = f"{cmd} {package['name']}"
-            output = ""
+        for platform in image["platforms"]:
+            regexInstalled = systems[image["system"]]["regex"]
+            cmd = systems[image["system"]]["cmd"]
+            tag = f"{args.tag}{image['tag_suffix']}"
             try:
-                output = client.containers.run(
-                    f"{image['image']}:{tag}",
-                    command,
-                    entrypoint="",
-                    platform=image["platform"],
-                    auto_remove=True,
-                    detach=False,
+                i = client.images.get(f"{image['image']}:{tag}")
+                ## check if the image is for the correct platform
+                if platform != i.attrs["Os"]:
+                    raise docker.errors.ImageNotFound(
+                        f"Image {image['image']}:{tag} is not for platform {platform}, found {i.attrs['Os']}"
+                    )
+            except docker.errors.ImageNotFound:
+                ## pull the image
+                logger.debug(f"Pulling image {image['image']}:{tag} for platform {platform}")
+                i = client.images.pull(repository=image["image"], tag=tag, platform=platform)
+                logger.debug(f"Image {i.id} pulled successfully")
+            for package in image["packages"]:
+                command = f"{cmd} {package['name']}"
+                output = ""
+                try:
+                    output = client.containers.run(
+                        f"{image['image']}:{tag}",
+                        command,
+                        entrypoint="",
+                        platform=platform,
+                        auto_remove=True,
+                        detach=False,
+                    )
+                except (docker.errors.ContainerError, docker.errors.NotFound) as e:
+                    logger.error(f"{e}, retrying")
+                    output = client.containers.run(
+                        f"{image['image']}:{tag}",
+                        command,
+                        entrypoint="",
+                        platform=platform,
+                        auto_remove=True,
+                        detach=False,
+                    )
+                result = re.search(regexInstalled, output.decode("utf-8").strip())
+                assert result, logger.error(
+                    f"{package['name']} not found in {image['image']}, output: {output.decode('utf-8').strip()}"
                 )
-            except (docker.errors.ContainerError, docker.errors.NotFound) as e:
-                logger.error(f"{e}, retrying")
-                output = client.containers.run(
-                    f"{image['image']}:{tag}",
-                    command,
-                    entrypoint="",
-                    platform=image["platform"],
-                    auto_remove=True,
-                    detach=False,
+                assert result.group(2).startswith(package["version"]), logger.error(
+                    f"{package['name']} version {package['version']} does not match {result.group(2)}"
                 )
-            result = re.search(regexInstalled, output.decode("utf-8").strip())
-            assert result, logger.error(
-                f"{package['name']} not found in {image['image']}, output: {output.decode('utf-8').strip()}"
-            )
-            assert result.group(2).startswith(package["version"]), logger.error(
-                f"{package['name']} version {package['version']} does not match {result.group(2)}"
-            )
-            logger.info(f"{image["image"]}, {result.group(1)}, {result.group(2)}")
+                logger.info(f"{image["image"]}, {result.group(1)}, {result.group(2)}, {result.group(3)}")
