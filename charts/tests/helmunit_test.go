bf203924a9547544ed318f6cf2899fa70aa44fa6
mohamad aldawamneh <m.aldawamneh@f5.com>
2025-08-27 10:34:39 +0100

    fixed make cover issue

--- charts/tests/helmunit_test.go
+++ charts/tests/helmunit_test.go
@@ -181,7 +181,7 @@ func TestHelmNICTemplateNegative(t *testing.T) {
 			valuesFile:        "testdata/startupstatus-invalid.yaml",
 			releaseName:       "startupstatus-invalid",
 			namespace:         "default",
-			expectedErrorMsgs: []string{"port is required", "path is required"},
+			expectedErrorMsgs: []string{"missing properties 'port', 'path'"},
 		},
 		"globalConfigInvalidFormat": {
 			valuesFile:        "testdata/global-config-invalid-format.yaml",
