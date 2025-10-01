7c7d25d7f2fe2eec86ab078dff957ca08458dfd4
Paul Abel <p.abel@f5.com>
2025-10-01 17:27:43 +0100

    set default values to script

--- .github/scripts/binary-json.sh
+++ .github/scripts/binary-json.sh
@@ -2,8 +2,9 @@
 
 set -eo pipefail
 
-path=$1
-project=$2
+path=${1:-dist/}
+project=${2:-kubernetes-ingress}
+binary_name=${3:-nginx-ingress}
 
 if [ -z "$path" ] || [ -z "$project" ]; then
   echo "Usage: $0 <path> <project>"
@@ -12,7 +13,7 @@ fi
 
 
 json='[]'
-for bin in $(find "$path" -type f -name "nginx-ingress"); do
+for bin in $(find "$path" -type f -name "$binary_name"); do
   dir=$(basename "$(dirname $bin)")
   if [[ "$dir" =~ ${project}_([a-zA-Z0-9]+)_([a-zA-Z0-9]+) ]]; then
     os="${BASH_REMATCH[1]}"
