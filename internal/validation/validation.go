ab797b32a054db9218a80dce84bb3860a3e5d815
Paul Abel <p.abel@f5.com>
2025-10-22 11:10:41 +0100

    Add resource reference validation function

--- internal/validation/validation.go
+++ internal/validation/validation.go
@@ -178,3 +178,8 @@ func ValidateURI(uri string, options ...URIValidationOption) error {
 
 	return nil
 }
+
+// HasNamespace checks if the given string is a resource reference with a namespace (i.e., has a '/' character).
+func HasNamespace(s string) bool {
+	return strings.Contains(s, "/")
+}
