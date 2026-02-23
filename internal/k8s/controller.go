172a9748dc5a45538b6e1ec365fc9850c030d609
Sean Breen <s.breen@f5.com>
2026-02-23 16:30:45 +0000

    fix copilot suggestion, move policy processing together in controller.go

--- internal/k8s/controller.go
+++ internal/k8s/controller.go
@@ -2171,17 +2171,6 @@ func (lbc *LoadBalancerController) createIngressEx(ing *networking.Ingress, vali
 		ingEx.SecretRefs[secretName] = secretRef
 	}
 
-	var policyRefs []conf_v1.PolicyReference
-	if ingEx.Ingress.Annotations[configs.PoliciesAnnotation] != "" {
-		policyRefs = k8spolicies.GetPolicyRefsFromAnnotation(ingEx.Ingress.Annotations[configs.PoliciesAnnotation], ing.Namespace)
-	}
-	policies, policyErrors := lbc.getPolicies(policyRefs, ing.Namespace)
-	if len(policyErrors) > 0 {
-		for _, err := range policyErrors {
-			nl.Warnf(lbc.Logger, "Error trying to get the policies for Ingress %v/%v: %v", ing.Namespace, ing.Name, err)
-		}
-	}
-
 	if basicAuth, exists := ingEx.Ingress.Annotations[configs.BasicAuthSecretAnnotation]; exists {
 		secretName := basicAuth
 		secretKey := ing.Namespace + "/" + secretName
@@ -2194,9 +2183,22 @@ func (lbc *LoadBalancerController) createIngressEx(ing *networking.Ingress, vali
 		ingEx.SecretRefs[secretName] = secretRef
 	}
 
-	if policies, exists := ingEx.Ingress.Annotations[configs.PoliciesAnnotation]; exists {
-		policyNames := strings.Split(policies, ",")
-		for _, policyName := range policyNames {
+	var policyNames string
+	var policyRefs []conf_v1.PolicyReference
+	if ingEx.Ingress.Annotations[configs.PoliciesAnnotation] != "" {
+		policyNames = ingEx.Ingress.Annotations[configs.PoliciesAnnotation]
+		policyRefs = k8spolicies.GetPolicyRefsFromAnnotation(policyNames, ing.Namespace)
+	}
+	policies, policyErrors := lbc.getPolicies(policyRefs, ing.Namespace)
+	if len(policyErrors) > 0 {
+		for _, err := range policyErrors {
+			nl.Warnf(lbc.Logger, "Error trying to get the policies for Ingress %v/%v: %v", ing.Namespace, ing.Name, err)
+		}
+	}
+
+	if policyNames != "" {
+		allPolicies := strings.Split(policyNames, ",")
+		for _, policyName := range allPolicies {
 			policyName = strings.TrimSpace(policyName)
 			if policyName == "" {
 				continue
