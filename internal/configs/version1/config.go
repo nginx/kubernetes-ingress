248406a6031b6e50370b0f51499627f888987b40
Haywood Shannon <5781935+haywoodsh@users.noreply.github.com>
2026-04-20 15:28:49 +0100

    move default server from nginx.conf to conf.d folder

--- internal/configs/version1/config.go
+++ internal/configs/version1/config.go
@@ -83,9 +83,15 @@ type Server struct {
 	AddHeaderInherit       string
 	ServerSnippets         []string
 	Name                   string
+	IsDefaultServer        bool
+	AccessLogOff           bool
+	DefaultServerReturn    string
+	HealthStatus           bool
+	HealthStatusURI        string
 	ServerTokens           string
 	Locations              []Location
 	EgressMTLS             *version2.EgressMTLS
+	HasRootLocation        bool
 	SSL                    bool
 	SSLCertificate         string
 	SSLCertificateKey      string
@@ -279,14 +285,8 @@ type MGMTConfig struct {
 type MainConfig struct {
 	AccessLog                          string
 	AddHeaderInherit                   string
-	DefaultServerAccessLogOff          bool
-	DefaultServerReturn                string
 	DisableIPV6                        bool
-	DefaultHTTPListenerPort            int
-	DefaultHTTPSListenerPort           int
 	ErrorLogLevel                      string
-	HealthStatus                       bool
-	HealthStatusURI                    string
 	HTTP2                              bool
 	HTTPSnippets                       []string
 	AddHeaders                         []version2.AddHeader
@@ -310,16 +310,12 @@ type MainConfig struct {
 	ResolverIPV6                       bool
 	ResolverTimeout                    string
 	ResolverValid                      string
-	RealIPHeader                       string
-	RealIPRecursive                    bool
 	SetRealIPFrom                      []string
 	ServerNamesHashBucketSize          string
 	ServerNamesHashMaxSize             string
 	MapHashBucketSize                  string
 	MapHashMaxSize                     string
 	ClientBodyBufferSize               string
-	ServerTokens                       string
-	SSLRejectHandshake                 bool
 	SSLCiphers                         string
 	SSLDHParam                         string
 	SSLPreferServerCiphers             bool
