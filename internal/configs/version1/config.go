2e899cca1f218ae9daf87d9400bc3c5fda0c0822
Haywood Shannon <5781935+haywoodsh@users.noreply.github.com>
2026-04-30 09:28:07 +0100

    simplify template

--- internal/configs/version1/config.go
+++ internal/configs/version1/config.go
@@ -91,7 +91,6 @@ type Server struct {
 	ServerTokens           string
 	Locations              []Location
 	EgressMTLS             *version2.EgressMTLS
-	HasRootLocation        bool
 	SSL                    bool
 	SSLCertificate         string
 	SSLCertificateKey      string
