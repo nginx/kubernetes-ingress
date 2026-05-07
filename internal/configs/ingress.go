865903af77882920afcbadea2e95ca1110d803f5
Haywood Shannon <5781935+haywoodsh@users.noreply.github.com>
2026-05-07 14:07:16 +0100

    fix rebase

--- internal/configs/ingress.go
+++ internal/configs/ingress.go
@@ -427,7 +427,6 @@ func generateNginxCfg(ncp NginxCfgParams) (version1.IngressNginxConfig, Warnings
 			server.SSLCertificateKey = DefaultServerSecretPath
 			server.SSLRejectHandshake = ncp.staticParams.SSLRejectHandshake
 			server.AccessLogOff = cfgParams.DefaultServerAccessLogOff
-			server.DefaultServerReturn = cfgParams.DefaultServerReturn
 			server.HealthStatus = ncp.staticParams.HealthStatus
 			server.HealthStatusURI = ncp.staticParams.HealthStatusURI
 		}
@@ -681,6 +680,10 @@ func generateNginxCfg(ncp NginxCfgParams) (version1.IngressNginxConfig, Warnings
 
 		if len(locations) == 0 {
 			grpcOnly = false
+		}
+
+		if isDefaultServer && !rootLocation {
+			server.DefaultServerReturn = cfgParams.DefaultServerReturn
 		}
 
 		server.Locations = locations
