7798c0a4ae3562bc09a6ce56c38c70bac710abea
Andy Bird <abird@zendesk.com>
2026-09-24 18:01:27 +0100

    fix: resolve mangled test from rebase and update error message for namespace not watched

--- internal/k8s/controller.go
+++ internal/k8s/controller.go
@@ -4701,7 +4701,7 @@ func (lbc *LoadBalancerController) getServiceForIngressBackend(backend *networki
 
 	nsi := lbc.getNamespacedInformer(namespace)
 	if nsi == nil {
-		return nil, fmt.Errorf("service %s doesn't exist", svcKey)
+		return nil, fmt.Errorf("namespace %s is not watched", namespace)
 	}
 	svcObj, svcExists, err = nsi.svcLister.GetByKey(svcKey)
 	if err != nil {
@@ -4720,7 +4720,7 @@ func (lbc *LoadBalancerController) getServiceForIngressBackend(backend *networki
 func (lbc *LoadBalancerController) getServiceFromInformer(namespace, serviceName string) (*api_v1.Service, error) {
 	nsi := lbc.getNamespacedInformer(namespace)
 	if nsi == nil {
-		return nil, fmt.Errorf("namespace %s is not being watched", namespace)
+		return nil, fmt.Errorf("namespace %s is not watched", namespace)
 	}
 
 	svcKey := namespace + "/" + serviceName
